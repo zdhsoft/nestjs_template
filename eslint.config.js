07586302e10f3c5e871bf9d916a129b8a490e8d1
rosesmall2010 <zdhsoft@gmail.com>
2025-02-27 12:05:50 +0800

    refactor: 去除ESM 的支持

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.js', 'test/**/*.ts'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -17,8 +17,8 @@ export default tseslint.config(
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
-      sourceType: 'module',
+      ecmaVersion: 2020, // 使用 ECMAScript 2020 版本
+      sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
@@ -29,7 +29,16 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true, // 语句后必须加分号
+          singleQuote: true, // 字符串使用单引号
+          tabWidth: 4, // 缩进使用 4 个空格
+          printWidth: 160, // 每行最大字符数为 160
+        },
+      ],
     },
   },
 );
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module.js";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
+function test() {
+    const a = {
+        name: 'rex',
+        age: 25,
+        address: {
+            city: 'Beijing',
+        },
+    };
+
+    console.log(a.address.city);
+
+    const b = 1;
+    const cc = 2;
+    const ddd = 5;
+
+    console.log(b + cc + ddd);
+}
+
+test();
